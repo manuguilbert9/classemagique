708a83ece14c3c750794761e0165127d33f73117
manuguilbert9 <manu.guilbert9@gmail.com>
2025-09-28 14:51:56 +0200

    Revert "Add messenger-style student chat with notifications"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,6 @@
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { UserProvider } from '@/context/user-context';
-import { ChatProvider } from '@/context/chat-context';
-import { ChatOverlay } from '@/components/chat/chat-overlay';
-import { ChatNotifications } from '@/components/chat/chat-notifications';
 
 export default function RootLayout({
   children,
@@ -15,20 +12,16 @@ export default function RootLayout({
 }>) {
   return (
     <UserProvider>
-      <ChatProvider>
-        <html lang="fr">
-          <head>
-            <title>Classe Magique</title>
-            <meta name="description" content="Des exercices amusants et engageants pour développer vos compétences !" />
-          </head>
-          <body className="font-body antialiased">
-            {children}
-            <ChatOverlay />
-            <ChatNotifications />
-            <Toaster />
-          </body>
-        </html>
-      </ChatProvider>
+      <html lang="fr">
+        <head>
+          <title>Classe Magique</title>
+          <meta name="description" content="Des exercices amusants et engageants pour développer vos compétences !" />
+        </head>
+        <body className="font-body antialiased">
+          {children}
+          <Toaster />
+        </body>
+      </html>
     </UserProvider>
   );
 }
