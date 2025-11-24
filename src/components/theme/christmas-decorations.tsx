7d8afdb519c7ceeb1e0608fa448ea39094734b75
manuguilbert9 <manu.guilbert9@gmail.com>
2025-11-25 00:13:20 +0100

    fix: refine Christmas theme integration (transparency, layering)

--- src/components/theme/christmas-decorations.tsx
+++ src/components/theme/christmas-decorations.tsx
@@ -9,9 +9,9 @@ export function ChristmasDecorations() {
     if (theme !== 'christmas') return null;
 
     return (
-        <div className="fixed inset-0 pointer-events-none z-40 overflow-hidden">
+        <div className="fixed inset-0 pointer-events-none z-0 overflow-hidden">
             {/* Top Left Corner */}
-            <div className="absolute top-0 left-0 w-32 h-32 md:w-48 md:h-48">
+            <div className="absolute top-0 left-0 w-32 h-32 md:w-48 md:h-48 mix-blend-multiply">
                 <Image
                     src="/images/theme/holly-corner.png"
                     alt="Decoration"
@@ -21,7 +21,7 @@ export function ChristmasDecorations() {
             </div>
 
             {/* Top Right Corner - Flipped */}
-            <div className="absolute top-0 right-0 w-32 h-32 md:w-48 md:h-48 transform scale-x-[-1]">
+            <div className="absolute top-0 right-0 w-32 h-32 md:w-48 md:h-48 transform scale-x-[-1] mix-blend-multiply">
                 <Image
                     src="/images/theme/holly-corner.png"
                     alt="Decoration"
@@ -31,7 +31,7 @@ export function ChristmasDecorations() {
             </div>
 
             {/* Top Center Garland */}
-            <div className="absolute top-0 left-1/2 -translate-x-1/2 w-full max-w-4xl h-24 md:h-32">
+            <div className="absolute top-0 left-1/2 -translate-x-1/2 w-full max-w-4xl h-24 md:h-32 mix-blend-multiply">
                 <Image
                     src="/images/theme/garland-top.png"
                     alt="Garland"
@@ -41,7 +41,7 @@ export function ChristmasDecorations() {
             </div>
 
             {/* Bottom Winter Scene */}
-            <div className="absolute bottom-0 left-0 w-full h-32 md:h-48 opacity-60">
+            <div className="absolute bottom-0 left-0 w-full h-32 md:h-48 opacity-60 mix-blend-multiply">
                 <Image
                     src="/images/theme/winter-scene-bottom.png"
                     alt="Winter Scene"
